532432fc2efcd1ef3cc15135f36f151894e35f6c
Eric <zhhui0714@126.com>
2025-03-29 15:12:47 +0800

    1111

--- Milestone/Views/FolderListView.swift
+++ Milestone/Views/FolderListView.swift
@@ -5,36 +5,18 @@ struct FolderListView: View {
     
     @Environment(\.modelContext) private var modelContext
     
-    @State private var currentEditingFolder: Folder?
-    
     var folders: [Folder]
     var isEditMode: Bool
     
     var body: some View {
-        List(folders) { folder in
-            FolderItemView(folder: folder, isEditMode: isEditMode)
-                .listRowSeparator(.hidden)
-                .listRowInsets(EdgeInsets())
-                .swipeActions(edge: .trailing) {
-                    Button(role: .destructive) {
-                        modelContext.delete(folder)
-                        try? modelContext.save()
-                    } label: {
-                        Label("删除", systemImage: "trash")
-                    }
-                    
-                    Button {
-                        currentEditingFolder = folder
-                    } label: {
-                        Label("编辑", systemImage: "pencil")
-                    }
-                    .tint(.blue)
-                }
+        List {
+            ForEach(folders, id: \.self) { folder in
+                FolderItemView(folder: folder, isEditMode: isEditMode)
+            }
+            .listRowSeparator(.hidden)
+            .listRowInsets(EdgeInsets())
         }
         .listRowSpacing(10)
         .listStyle(.plain)
-        .sheet(item: $currentEditingFolder) { folder in
-            FolderEditView(folder: folder)
-        }
     }
 }
